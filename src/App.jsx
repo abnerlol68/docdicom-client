b0f370d02ef588af31f4390ca6d08a3296f1f090
Abner Perez Rodriguez <abnerstr81@gmail.com>
2023-04-07 15:22:24 -0600

    Layouts and its routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,29 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import RtlLayout from "layouts/rtl";
 import AdminLayout from "layouts/admin";
 import AuthLayout from "layouts/auth";
+
+// App's layouts Imports
+import Dashboard from "app/layouts/dashboard";
+import Landing from "app/layouts/landing";
+import Login from "app/layouts/login";
+import Signup from "app/layouts/signup";
+import NotFound from "app/layouts/not-found";
+
 const App = () => {
   return (
     <Routes>
+      {/* App */}
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      <Route path="dashboard/*" element={<Dashboard />} />
+      <Route path="landing/*" element={<Landing />} />
+      <Route path="login/*" element={<Login />} />
+      <Route path="signup/*" element={<Signup />} />
+      <Route path="not-found/*" element={<NotFound />} />
+
       <Route path="auth/*" element={<AuthLayout />} />
       <Route path="admin/*" element={<AdminLayout />} />
       <Route path="rtl/*" element={<RtlLayout />} />
-      <Route path="/" element={<Navigate to="/admin" replace />} />
+      {/* <Route path="/" element={<Navigate to="/admin" replace />} /> */}
     </Routes>
   );
 };
---
+++ src/app/layouts/not-found/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+
+export default function NotFound(props) {
+  document.documentElement.dir = "ltr";
+
+  return (
+    <div className="flex h-full w-full">
+      <button className="rounded-xl border-2 border-blue-500 px-5 py-3 text-base font-medium text-blue-500 transition duration-200 hover:bg-blue-600/5 active:bg-blue-700/5 dark:border-blue-400 dark:bg-blue-400/10 dark:text-white dark:hover:bg-blue-300/10 dark:active:bg-blue-200/10">
+        Error 404 Not Found
+      </button>
+    </div>
+  );
+}
