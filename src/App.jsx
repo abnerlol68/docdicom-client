cb0ab6ab90aada4a7cbf16fdb4e84bad30509c04
JorshBG <jordiyair29@gmail.com>
2023-04-11 17:49:06 -0600

    Add the layout and views of error to the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 
 import RtlLayout from "layouts/rtl";
 import AdminLayout from "layouts/admin";
 import AuthLayout from "layouts/auth";
+import ErrorLayout from "layouts/error";
+
 const App = () => {
-  return (
-    <Routes>
-      <Route path="auth/*" element={<AuthLayout />} />
-      <Route path="admin/*" element={<AdminLayout />} />
-      <Route path="rtl/*" element={<RtlLayout />} />
-      <Route path="/" element={<Navigate to="/admin" replace />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="auth/*" element={<AuthLayout/>}  />
+            <Route path="admin/*" element={<AdminLayout/>}/>
+            <Route path="rtl/*" element={<RtlLayout/>}/>
+            <Route path="error/*" element={<ErrorLayout/>}/>
+            <Route path="/" element={<Navigate to="/admin" replace/>}/>
+            <Route path="*" element={<ErrorLayout/>}/>
+        </Routes>
+    );
 };
 
 export default App;
