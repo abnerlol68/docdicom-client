f4f14369045f16e19dfe40a88761b9b35bcac662
Abner Perez Rodriguez <abnerstr81@gmail.com>
2023-05-23 12:56:36 -0600

    Add disabled option in SelectField

--- src/components/fields/SelectField.jsx
+++ src/components/fields/SelectField.jsx
@@ -9,30 +9,37 @@ export default function SelectField({
   val,
   change,
   options,
+  disabled,
 }) {
   return (
-    <div className={`${extra}`}>
+    <div className={`flex flex-col ${extra}`}>
       <label
         htmlFor={id}
-        className={`text-base text-navy-700 dark:text-white ${
+        className={`text-base text-navy-700 dark:text-white mb-2 ${
           variant === "auth" ? "ml-1.5 font-medium" : "ml-3 font-bold"
         } ${extraLabel}`}
       >
         {label}
       </label>
-      <br />
       <select
-        className={`select text-base select-bordered w-full max-w-xs rounded-xl border bg-white/0 p-3 outline-none dark:bg-navy-900 focus:border-indigo-500 ${extraSelect}`}
+        className={
+          disabled
+            ? "w-full max-w-xs rounded-xl border bg-gray-200 p-3 text-base outline-none"
+            : `select select-bordered w-full max-w-xs rounded-xl border bg-white/0 p-3 text-base outline-none focus:border-indigo-500 dark:bg-navy-900 ${extraSelect}`
+        }
         value={val}
         onChange={change}
         name={name}
         id={id}
+        disabled={disabled}
       >
         <option disabled selected>
           {label}
         </option>
         {Object.entries(options).map(([altKey, altVal]) => (
-          <option key={altKey} value={altKey}>{altVal}</option>
+          <option key={altKey} value={altKey}>
+            {altVal}
+          </option>
         ))}
       </select>
     </div>
